ba927548c952d8c92077c982e5906ef36e8b33d2
Mikael37-str <peraltamicaelbautista@gmail.com>
2025-04-16 17:22:05 -0600

    NJS2

---
+++ PDeISC/NodeJS-01/NJS2/ejercicio1md2.js
@@ -0,0 +1,16 @@
+// Creamos la función para la suma y la exportamos
+export function suma (a,b){
+    return (a+b);
+}
+// Creamos la función para la resta y la exportamos
+export function resta (a,b){
+    return (a-b);
+}
+// Creamos la función para la multiplicación y la exportamos
+export function multipli (a,b){
+    return (a*b);
+}
+// Creamos la función para la divición y la exportamos
+export function dividi (a,b){
+    return (a/b);
+}
